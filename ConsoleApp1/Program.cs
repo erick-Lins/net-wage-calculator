235a0e98cde6ee703a16947920ee27d62a312700
Erick Lins <ericklinz_18@hotmail.com>
2024-06-17 18:43:13 -0300

    Employee: Renato Russo fired Employee: Roberto Carrara hired

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,11 +4,11 @@ class Program
     static void Main(string[] args)
     {
         Employee erickLins = new Employee("Matutinal", "Operator", 350, 80);
-        Employee renatoRusso = new Employee("Vespertine", "Manager", 490, 65);
+        Employee robertoCarrara = new Employee("Vespertine", "Manager", 490, 65);
         //erickLins.PrintInformation();
         //ShowInfosAbouJob(erickLins);
 
-        renatoRusso.PrintInformation();
-        renatoRusso.ShowInfosAbouJob();
+        robertoCarrara.PrintInformation();
+        robertoCarrara.ShowInfosAbouJob();
     }
 }
